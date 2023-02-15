291d5d50e743d2982d9dc4d5c48719466688ab6a
Emiliano.m.s <cabeemil86@gmail.com>
2023-02-15 12:43:45 +0100

    Primer commit: Estado final

--- Alumnos/src/Alumno/Alumno.java
+++ Alumnos/src/Alumno/Alumno.java
@@ -7,7 +7,39 @@ package Alumno;
  */
 
 public class Alumno {
-  public String nombre;
-  public double notaMedia = 0.0;
+
+    /**
+     * @return devuelve el nombre de los alumnos
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre asigna el nobre a cada alumno
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return devuelve la nota de cada alumno
+     */
+    public double getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the notaMedia to set
+     */
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    /**
+     * refactorizamos cambiando el nombre de notaMedia por nota
+     * que es la que tiene cada alumno
+     */
+  private String nombre;
+  private double nota = 0.0;
   
 }
--- Alumnos/src/alumnos/Alumnos.java
+++ Alumnos/src/alumnos/Alumnos.java
@@ -14,37 +14,61 @@ public class Alumnos {
     // Pide los datos de los alumnos /////////////////////////////////
         
     System.out.println("A continuacion debera introducir el nombre y la nota media de 5 alumnos.");
-
-    String nombreIntroducido;
-    double notaIntroducida;
-    
-    for(int i = 0; i < 5; i++) {
-      
-      alum[i] = new Alumno();          
-      System.out.println("Alumno " + i);
-      System.out.print("Nombre: ");
-      nombreIntroducido = entrada.nextLine();
-      (alum[i]).nombre = nombreIntroducido;
-      System.out.print("Nota media: ");
-      notaIntroducida = Double.parseDouble(entrada.nextLine());
-      alum[i].notaMedia = notaIntroducida;
-    } // for i
+/**
+ * Llamamos al metodo para ingresar los datos de los alumnos
+ */
+        ingresaAlumnos(alum, entrada);
     
     // Muestra los datos de los alumnos /////////////////////////////////
     
     System.out.println("Los datos introducidos son los siguientes:");
-
-    double  sumaDeMedias = 0;
+/**
+ * Mostramos los datos de los alumnos y la suma de todas las notas
+ */
+    double  sumaNotasAlumnos = muestraDatos(alum);
     
-    for(int i = 0; i < 5; i++) {
-      System.out.println("Alumno " + i);  
-      System.out.println("Nombre: " + alum[i].nombre);    
-      System.out.println("Nota media: " + alum[i].notaMedia);
-      System.out.println("----------------------------");
-
-      sumaDeMedias += alum[i].notaMedia;
-    } // for i
-    
-    System.out.println("La media global de la clase es " + sumaDeMedias / 5);
+    System.out.println("La media global de la clase es " + sumaNotasAlumnos / 5);
   }
+/**
+ * 
+ * @param asigna los datos de cada alumno
+ * @return muestra la informacion de cada alumno
+ */
+    private static double muestraDatos(Alumno[] alum) {
+        double  sumaDeMedias = 0;
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Alumno " + i);
+            System.out.println("Nombre: " + alum[i].getNombre());
+            System.out.println("Nota media: " + alum[i].getNota());
+            System.out.println("----------------------------");
+            
+            sumaDeMedias += alum[i].getNota();
+        } // for i
+        return sumaDeMedias;
+    }
+/**
+ * Este metodo
+ * Crea los objetos alumno y le asignamos los datos de cada uno de ellos  
+ * 
+ * @param alum 
+ *
+ * @param entrada ingresa los datos de los alumnos
+ * @throws NumberFormatException 
+ */
+    private static void ingresaAlumnos(Alumno[] alum, Scanner entrada) throws NumberFormatException {
+        String nombreIntroducido;
+        double notaIntroducida;
+        
+        for(int i = 0; i < 5; i++) {
+            
+            alum[i] = new Alumno();
+            System.out.println("Alumno " + i);
+            System.out.print("Nombre: ");
+            nombreIntroducido = entrada.nextLine();
+            (alum[i]).setNombre(nombreIntroducido);
+            System.out.print("Nota media: ");
+            notaIntroducida = Double.parseDouble(entrada.nextLine());
+            alum[i].setNota(notaIntroducida);
+        } 
+    }
 }
